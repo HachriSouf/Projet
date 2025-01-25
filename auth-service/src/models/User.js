3ba9c73eff91d910bbd7a9884b23982dddedab94
adamed14 <adamedekaki1312@gmail.com>
2025-01-25 23:59:21 +0100

    changement

--- auth-service/src/models/User.js
+++ auth-service/src/models/User.js
@@ -5,7 +5,13 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  refreshToken: { type: String, default: null }
+  refreshToken: { type: String, default: null },
+  registrationToken: { type: String, default: null },
+  registratedAt: { type: Date, default: null },
+  lastSignedInAt: { type: Date, default: null },
+  createdAt: { type: Date, default: null },
+  updatedAt: { type: Date, default: null },
+  deletedAt: { type: Date, default: null }
 });
 
 
